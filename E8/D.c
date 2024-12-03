d1731c01216876e997eb83744aaeffd4c63e374b
winter676 <24421017@buaa.edu.cn>
2024-12-03 20:53:58 +0800

    1

---
+++ E8/D.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+struct Move {
+    long long list;
+    int isUsed;
+};
+
+long long findMinIndex(struct Move moving[],long long n,long long m);
+
+int main()
+{
+    struct Move moving[1000];
+    long long n,m,sum = 0,min = 0,minIndex = 0;
+
+    scanf("%lld%lld",&n,&m);
+    for (long long i = 0; i < n; i++ ){
+        moving[i].isUsed = 0;
+        scanf("%lld",&moving[i].list);
+    }
+    for (long long i = 0; i < n; i++ ){
+        minIndex = findMinIndex(moving,n,m);
+        long long value = moving[minIndex].list-m;
+        long long valueAbs = llabs(value);
+        sum += valueAbs;
+        m += value;
+        printf("%lld ",m);
+        moving[minIndex].isUsed = 1;
+    }
+    printf("\n%lld\n",sum);
+    return 0;
+}
+
+
+long long findMinIndex(struct Move moving[],long long n,long long m)
+{
+    long long min = -1;
+    long long minIndex = -1;
+
+    for (long long i =  0; i < n; i++ ){
+        if (moving[i].isUsed){
+            continue;
+        }
+        long long value = llabs(moving[i].list - m);
+        if (min == -1 || value < min  || (value == min && moving[i].list < moving[minIndex].list)){
+            min = value;
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
--- 模板.c
+++ 模板.c
@@ -160,3 +160,104 @@ void optiBubSort(int a[], int n)
             break;
     }
 }
+
+
+
+//排序算法cmp函数；
+//从小到大：
+int cmp(const void *a ,const void *b)
+{
+	return *(int *)a - *(int *)b ;			//从小到大排序，不能写成 return a < b 这种形式
+}
+
+//从大到小
+int cmp(const void *a ,const void *b)
+{
+	return *(int *)b - *(int *)a ;
+}
+
+
+
+//二分算法
+//左闭右闭
+int search(int nums[], int size, int target) //nums是数组，size是数组的大小，target是需要查找的值
+{
+    int left = 0;
+    int right = size - 1;	// 定义了target在左闭右闭的区间内，[left, right]
+    while (left <= right) {	//当left == right时，区间[left, right]仍然有效
+        int middle = left + ((right - left) / 2);//等同于 (left + right) / 2，防止溢出
+        if (nums[middle] > target) {
+            right = middle - 1;	//target在左区间，所以[left, middle - 1]
+        } else if (nums[middle] < target) {
+            left = middle + 1;	//target在右区间，所以[middle + 1, right]
+        } else {	//既不在左边，也不在右边，那就是找到答案了
+            return middle;
+        }
+    }
+    //没有找到目标值
+    return -1;
+}
+
+//左闭右开
+int search(int nums[], int size, int target)
+{
+	int left = 0;
+	int right = size; //定义target在左闭右开的区间里，即[left, right)
+	while (left < right) {	//因为left = right的时候，在[left, right)区间上无意义
+		int middle = left + ((right - left) / 2);
+		if (nums[middle] > target) {
+			right = middle; //target 在左区间，在[left, middle)中 
+		} else if (nums[middle] < target) {
+			left = middle + 1;
+		} else {
+			return middle;
+		}
+	} 
+    // 没找到就返回-1
+	return -1;
+}
+
+
+//进制的转换
+int char_to_num(char ch) //字符串到数字
+{
+    if(ch>='0' && ch<='9')
+        return ch-'0';  /*将数字字符转换成数字*/
+    else
+        return ch-'A'+10;  /*将字母字符转换成数字*/
+}
+
+
+char num_to_char(int num)//数字到数字
+{
+    if(num>=0 && num<=9)
+        return (char)('0'+num-0);  /*将0~9之间的数字转换成字符*/
+    else
+        return (char)('A'+num-10);  /*将大于10的数字转换成字符*/
+}
+
+
+long source_to_decimal(char temp[], int source) //任意进制数转换为10进制
+{
+    long decimal_num = 0;  /*存储展开之后的和*/
+    int length;
+    int i;
+    for( i=0; temp[i]!='\0'; i++ );
+    length=i;
+    for( i=0; i<=length-1; i++ )  /*累加*/
+        decimal_num = (decimal_num*source) + char_to_num(temp[i]);
+    return decimal_num;
+}
+
+int decimal_to_object(char temp[], long decimal_num, int object)
+{
+    int i=0;
+    while(decimal_num)
+    {
+        temp[i] = num_to_char(decimal_num % object);  /*求出余数并转换为字符*/
+        decimal_num = decimal_num / object;  /*用十进制数除以基数*/
+        i++;
+    }
+    temp[i]='\0';
+    return i;
+}
